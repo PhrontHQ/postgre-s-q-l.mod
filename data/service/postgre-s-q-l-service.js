4b9e11ef4a9718711f0b1e3dfad43c1eb12b9bb9
Benoit Marchant <marchant@mac.com>
2025-12-17 18:31:34 +0100

    Fix typo in method name

--- data/service/postgre-s-q-l-service.js
+++ data/service/postgre-s-q-l-service.js
@@ -6318,7 +6318,7 @@ PostgreSQLService.addClassProperties({
         value: function (rawTransaction, transactionOperation, responseOperation) {
 
             // callback - checkout a client
-            this.connectForDataOperation(transactionOperation,(err, client, done) => {
+            this.connectForRawDataOperation(transactionOperation,(err, client, done) => {
                 
                 /*
                     If connection fails, there's not much more we can do, we report the error 
